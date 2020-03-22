4905c3b9ff6f797d26e37eb40d5c2b8bf2ba1af2
Chathuranga Sandun <chathurangasandunkumara@gmail.com>
2020-03-22 19:24:39 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -20,7 +20,7 @@ class MyApp extends StatelessWidget {
         // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: 'Home Page'),
+      home: MyHomePage(title: 'Home page test CI'),
     );
   }
 }
